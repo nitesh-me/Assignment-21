fbf2481295bc97dfd9f4358957ccfec9f54ce753
hestin-singh <hestinsingh@gmail.com>
2018-12-19 09:47:19 +0530

    second

--- Assignment-21.1/app.js
+++ Assignment-21.1/app.js
@@ -14,57 +14,129 @@ var collection = Obj.collection('employee');
 
 var empdata =[
     {"ename":"Vinod","department":"Clerical staff","designation":"Fresher","salary":4000, "dateofJoining":"3-october","city":"bangalore"},
-    {"ename":"Sunil","department":"Support staff","designation":"Fresher","salary":14000, "dateofJoining":"30-october","city":"Chennai"},
-    {"ename":"Sumit","department":"ops staff","designation":"Fresher","salary":40000, "dateofJoining":"07-november","city":"Mumbai"},
-    {"ename":"kundan","department":"development staff","designation":"software developer","salary":35000, "dateofJoining":"30-march","city":"bangalore"},
-    {"ename":"kapil","department":"management staff","designation":"manager","salary":31000," dateofJoining":"23-october","city":"Mumbai"},
-    {"ename":"nitesh","department":"logistic staff","designation":"","salary":35000, "dateofJoining":"5-october","city":"Delhi"},
-    {"ename":"Hestin","department":"Clerical staff","designation":"Fresher","salary":32000, "dateofJoining":"8-october","city":"Pune"},
-    {"ename":"Richa","department":"development staff","designation":"software developer","salary":4000, "dateofJoining":"1-Januay","city":"Pune"},
-    {"ename":"Kalpana","department":"Clerical staff","designation":"Fresher","salary":10000, "dateofJoining":"3-february","city":"Mumbai"},
-    {"ename":"lalit","department":"development staff","designation":"software developer","salary":40000, "dateofJoining":"4-march","city":"kerala"},
+    {"ename":"Sunil","department":"Support staff","designation":"Fresher","salary":14000, "dateofJoining":"30-october","city":"california"},
+    {"ename":"Sumit","department":"ops staff","designation":"Fresher","salary":40000, "dateofJoining":"07-november","city":"singapore"},
+    {"ename":"kundan","department":"development staff","designation":"software developer","salary":35000, "dateofJoining":"30-march","city":"newyork,"},
+    {"ename":"kapil","department":"management staff","designation":"manager","salary":31000," dateofJoining":"23-october","city":"newyork,"},
+    {"ename":"nitesh","department":"logistic staff","designation":"","salary":35000, "dateofJoining":"5-october","city":"delhi"},
+    {"ename":"Hestin","department":"Clerical staff","designation":"Fresher","salary":32000, "dateofJoining":"8-october","city":"delhi"},
+    {"ename":"Richa","department":"development staff","designation":"software developer","salary":4000, "dateofJoining":"1-Januay","city":"bangalore"},
+    {"ename":"Kalpana","department":"Clerical staff","designation":"Fresher","salary":10000, "dateofJoining":"3-february","city":"california"},
+    {"ename":"lalit","department":"development staff","designation":"software developer","salary":39000, "dateofJoining":"4-march","city":"newyork,"},
    
     
 ];
 // Inserting all document 
+console.log("hello world");
 collection.insertMany(empdata,(err,data)=>{
     if(err){
         console.log("sd");
     }
     else{
         console.log("data inserted properly", data);
+        
     }
 
 });
-console.log("hello world");
+
 // find and Display only designation of an Employee
-var particualrEmp =collection.find({"ename":"Vinod"});
-console.log(particualrEmp);
+
+var particualrEmp =collection.find({"ename":"Vinod"}).toArray((err,data)=>{
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log("find and Display only designation of an Employee");
+        console.log(data);
+    }
+})
+
 
 //find and Display the records of employee whose salary is greater than 7000
 
-// collection.find({"salary":{$gt:7000}},(err,data)=>{
-//     if(err){
-//         console.log("Error is ",err);
-//     }
-//     else{
-//         console.log(data);
-//     }
-// });
+
+collection.find({"salary":{$gt:7000}}).toArray((err,data)=>{
+    if(err){
+        console.log("Error is ",err);
+    }
+    else{
+        console.log("find and Display the records of employee whose salary is greater than 7000");
+        console.log(data);
+    }
+});
 
 
 //  Sort and Display the records of an employee based on the date of joining
-// collection.find({$sort :{dateofJoining:1}},(err,data)=>{
-//     if(err){
-//         console.log(err);
-//     }
-//     else{
-//         console.log(data);
-//     }
-// });
+collection.find({}).sort({"dateofJoining":1}).toArray((err,data)=>{
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log('Sort and Display the records of an employee based on the date of joining');
+        console.log(data);
+    }
+});
+//==================================================================
+// Sort and Display the records of an employee based on the salary in ascending order
+collection.find({}).sort({"salary":1}).toArray((err,data)=>{
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log('Sort and Display the records of an employee based on the salary in ascending order');
+        console.log(data);
+    }
+});
 
+// find and Display the records of an employee whose salary in between 5000 to 40000
+collection.find({"salary":{$gt:5000, $lt:40000}}).toArray((err,data)=>{
+    if(err){
+        console.log("Error is ",err);
+    }
+    else{
+        console.log("find and Display the records of employee whose salary is greater than 7000");
+        console.log(data);
+    }
+});
+//===================================find and display all the records of an employee except whose designation is developer.
 
+collection.find({"designation":"software developer"}).toArray((err,data)=>{
+    if(err){
+        console.log("Error is ",err);
+    }
+    else{
+        console.log("find and display all the records of an employee except whose designation is developer");
+        console.log(data);
+    }
+});
 
+//============   Sort and display the records of an employee's city in descending order ========//
+collection.find({}).sort({"city":1}).toArray((err,data)=>{
+    if(err){
+        console.log("Error is ",err);
+    }
+    else{
+        console.log("Sort and display the records of an employee's city in descending order");
+        console.log(data);
+    }
+});
+
+//==================find and display the records of an employee's whose city starts with Singapore and
+//===============salary is greater than 8000
+
+collection.find({"city":"singapore", salary:{$gt:8000}}).sort({"city":1}).toArray((err,data)=>{
+    if(err){
+        console.log("Error is ",err);
+    }
+    else{
+        console.log("Sort and display the records of an employee's city in descending order");
+        console.log(data);
+    }
+});
+// =========================Drop the employee records and display=============================
+collection.drop(()=>{
+    console.log("Colection dropped");
+})
 
 });
 
--- Assignment-21.2/app.js
+++ Assignment-21.2/app.js
@@ -7,9 +7,44 @@ const port = process.env.PORT || 8000;
 mongoose.connect("mongodb://localhost/emp",{ useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
+
+var empdata =[
+    {"ename":"Vinod","department":"Clerical staff","designation":"Fresher","salary":4000, "city":"bangalore"},
+    {"ename":"Sunil","department":"Support staff","designation":"Fresher","salary":14000,"city":"california"},
+    {"ename":"Sumit","department":"ops staff","designation":"Fresher","salary":40000,"city":"singapore"},
+    {"ename":"kundan","department":"development staff","designation":"software developer","salary":35000, "city":"newyork,"},
+    {"ename":"kapil","department":"management staff","designation":"manager","salary":31000,"city":"newyork,"},
+    {"ename":"nitesh","department":"logistic staff","designation":"","salary":35000, "city":"delhi"},
+    {"ename":"Hestin","department":"Clerical staff","designation":"Fresher","salary":32000,"city":"delhi"},
+    {"ename":"Richa","department":"development staff","designation":"software developer","salary":4000, "city":"bangalore"},
+    {"ename":"Kalpana","department":"Clerical staff","designation":"Fresher","salary":10000,"city":"california"},
+    {"ename":"lalit","department":"development staff","designation":"software developer","salary":39000, "city":"newyork,"},
+   
+    
+];
+
+Employee.insertMany(empdata,(err,data)=>{
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log(data);
+    }
+})
+
+Employee.remove({"designation":"software developer"},(err,data)=>{
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log("designation with software developer   removed    ");
+    }
+})
 app.get('/',(req,res)=>{
     res.send("jello world");
 });
+
+
 
 
 app.post('/',(req,res)=>{
